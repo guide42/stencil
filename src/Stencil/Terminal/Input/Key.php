b944c578acbc9a2ae314f72633dfdf9d02ce183f
Juan M <jm@guide42.com>
2013-10-21 18:17:49 +0200

    InputTerminal: Add modifiers

--- src/Stencil/Terminal/Input/Key.php
+++ src/Stencil/Terminal/Input/Key.php
@@ -7,6 +7,10 @@ namespace Stencil\Terminal\Input;
  */
 class Key
 {
+    const MOD_ALT  = 1;
+    const MOD_CTRL = 2;
+    const MOD_META = 4;
+
     /**
      * String representation of the Key pressed.
      *
@@ -15,15 +19,57 @@ class Key
     protected $repr;
 
     /**
+     * Modifiers bitmask for the Key.
+     *
+     * @var integer
+     */
+    protected $modifiers;
+
+    /**
      * @param string $repr
      */
-    public function __construct($repr)
+    public function __construct($repr, $modifiers = 0)
     {
         $this->repr = $repr;
+        $this->modifiers = $modifiers;
+    }
+
+    /**
+     * Return true if ALT key is pressed. False otherwise.
+     *
+     * @return boolean
+     */
+    public function isAltPressed()
+    {
+        return $this->modifiers & self::MOD_ALT !== 0;
+    }
+
+    /**
+     * Return true if CTRL key is pressed. False otherwise.
+     *
+     * @return boolean
+     */
+    public function isCtrlPressed()
+    {
+        return $this->modifiers & self::MOD_CTRL !== 0;
+    }
+
+    /**
+     * Return true if META key is pressed. False otherwise.
+     *
+     * @return boolean
+     */
+    public function isMetaPressed()
+    {
+        return $this->modifiers & self::MOD_META !== 0;
     }
 
     public function __toString()
     {
-        return $this->repr;
+        return sprintf('<%s(%s%s%s%s)>', get_class($this),
+            $this->isMetaPressed() ? 'meta+' : '',
+            $this->isCtrlPressed() ? 'ctrl+' : '',
+            $this->isAltPressed() ? 'alt+' : '',
+            $this->repr);
     }
 }
